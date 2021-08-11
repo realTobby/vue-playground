e6a95d6b4e7d0ef43d4a95d77b297499a1e22cc3
realTobby <tkattanek00@gmail.com>
2021-08-11 17:00:45 +0200

    Init
    Nice HelloWorld!

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vue-playground
-This repository represents a playground for the vueJS framework. Basically learning for my new job.
+# helloworld
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ helloworld/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://avatars.githubusercontent.com/u/24779607?s=400&u=dbcb6e3b012ee34756d114df139b5e62eb3a21b9&v=4">
+    <HelloWorld msg="VueJS? Follow me on GitHub lol: github.com/realTobby"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
